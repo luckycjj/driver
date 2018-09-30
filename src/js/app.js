00bd5a6e4584ccef23a8b6388afe65278316b487
luckycjj <512170894@qq.com>
2018-09-30 13:54:22 +0800

    分享

--- src/js/app.js
+++ src/js/app.js
@@ -494,7 +494,48 @@ var androidIos = {
       if(cval!=null){
         document.cookie= name + "="+cval+";expires="+exp.toGMTString();
       }
-    }
+    },
+  lineCanvas:function (obj) {
+    this.linewidth = 1;
+    this.color = "#000000";
+    this.background = "#ffffff";
+    for (var i in obj) {
+      this[i] = obj[i];
+    };
+    this.canvas = document.createElement("canvas");
+    this.el.appendChild(this.canvas);
+    this.cxt = this.canvas.getContext("2d");
+    this.canvas.width = this.el.clientWidth;
+    this.canvas.height = this.el.clientHeight;
+    this.cxt.fillStyle = this.background;
+    this.cxt.fillRect(0, 0, this.canvas.width, this.canvas.width);
+    this.cxt.strokeStyle = this.color;
+    this.cxt.lineWidth = this.linewidth;
+    this.cxt.lineCap = "round";
+    //开始绘制
+    this.canvas.addEventListener("touchstart", function(e) {
+      this.cxt.beginPath();
+      this.cxt.moveTo(e.changedTouches[0].pageX, e.changedTouches[0].pageY);
+    }.bind(this), false);
+    //绘制中
+    this.canvas.addEventListener("touchmove", function(e) {
+      this.cxt.lineTo(e.changedTouches[0].pageX, e.changedTouches[0].pageY);
+      this.cxt.stroke();
+    }.bind(this), false);
+    //结束绘制
+    this.canvas.addEventListener("touchend", function() {
+      this.cxt.closePath();
+    }.bind(this), false);
+    //清除画布
+    this.clearEl.addEventListener("click", function() {
+      this.cxt.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }.bind(this), false);
+    //保存图片，直接转base64
+    this.saveEl.addEventListener("click", function() {
+      var imgBase64 = this.canvas.toDataURL();
+      console.log(imgBase64);
+    }.bind(this), false);
+  }
 };
 export {
   androidIos
