c5e447981a419b2ee35906423af605e54e25d77d
luckycjj <512170894@qq.com>
2018-10-10 14:57:12 +0800

    解析二维码

--- src/components/track/Qrcode.vue
+++ src/components/track/Qrcode.vue
@@ -4,12 +4,14 @@
      <div id="title" v-if="ty == '1'" v-title data-title="签收二维码"></div>
      <div id="title" v-if="ty == '2'" v-title data-title="订单二维码"></div>
      <canvas id="canvas"></canvas>
+    <!-- <input type="file"  id="newfile" @change="jiexi($event)">-->
    </div>
 </template>
 
 <script>
   import QRCode from 'qrcode'
   import {androidIos} from "../../js/app";
+  import reqrcode from  '../../js/reqrcode'
   export default {
         name: "qrcode",
     data(){
@@ -32,12 +34,31 @@
         _this.ty = _this.$route.query.ty;
         _this.useqrcode();
       },
+      jiexi:function (e) {
+        var getObjectURL = function (file) {
+          var url = null;
+          if (window.createObjectURL != undefined) { // basic
+            url = window.createObjectURL(file);
+          } else if (window.URL != undefined) { // mozilla(firefox)
+            url = window.URL.createObjectURL(file);
+          } else if (window.webkitURL != undefined) { // webkit or chrome
+            url = window.webkitURL.createObjectURL(file);
+          }
+          return url;
+        }
+        console.log(getObjectURL(e.target.files[0]));// newfile[0]是通过input file上传的二维码图片文件
+        reqrcode.decode(getObjectURL(e.target.files[0]));
+        reqrcode.callback = function (imgMsg) {
+          alert("二维码解析：" + imgMsg)
+        }
+      },
      useqrcode:function(){
        //生成的二维码内容，可以添加变量
        // 0 交接二维码 1签收二维码 2订单二维码
        var json = {
          pk:this.QueryDetail,
-         type:this.$route.query.ty
+         type:this.$route.query.ty,
+         time:(new Date()).getTime()
        };
        json = JSON.stringify(json);
        var canvas = document.getElementById('canvas');
