d3c64d9cce1c2f78dd733e61ec748dc42827ea29
luckycjj <512170894@qq.com>
2018-10-11 10:12:44 +0800

    扫一扫

--- src/components/track/Qrcode.vue
+++ src/components/track/Qrcode.vue
@@ -4,7 +4,6 @@
      <div id="title" v-if="ty == '1'" v-title data-title="签收二维码"></div>
      <div id="title" v-if="ty == '2'" v-title data-title="订单二维码"></div>
      <canvas id="canvas"></canvas>
-    <!-- <input type="file"  id="newfile" @change="jiexi($event)">-->
    </div>
 </template>
 
@@ -33,24 +32,6 @@
         var _this = this;
         _this.ty = _this.$route.query.ty;
         _this.useqrcode();
-      },
-      jiexi:function (e) {
-        var getObjectURL = function (file) {
-          var url = null;
-          if (window.createObjectURL != undefined) { // basic
-            url = window.createObjectURL(file);
-          } else if (window.URL != undefined) { // mozilla(firefox)
-            url = window.URL.createObjectURL(file);
-          } else if (window.webkitURL != undefined) { // webkit or chrome
-            url = window.webkitURL.createObjectURL(file);
-          }
-          return url;
-        }
-        console.log(getObjectURL(e.target.files[0]));// newfile[0]是通过input file上传的二维码图片文件
-        reqrcode.decode(getObjectURL(e.target.files[0]));
-        reqrcode.callback = function (imgMsg) {
-          alert("二维码解析：" + imgMsg)
-        }
       },
      useqrcode:function(){
        //生成的二维码内容，可以添加变量
