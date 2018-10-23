eda00787dfcebc80e90704fe703e0074eceda456
luckycjj <512170894@qq.com>
2018-10-23 14:14:23 +0800

    自适应高X

--- src/components/track/map.vue
+++ src/components/track/map.vue
@@ -75,9 +75,8 @@
             center: [_this.carList.startJ, _this.carList.startW],//地图中心点
             zoom: 13 //地图显示的缩放级别
           });
-          AMap.plugin(['AMap.ToolBar','AMap.Scale'],
+          AMap.plugin(['AMap.Scale'],
             function(){
-              map.addControl(new AMap.ToolBar());
               map.addControl(new AMap.Scale());
             });
           //构造路线导航类
